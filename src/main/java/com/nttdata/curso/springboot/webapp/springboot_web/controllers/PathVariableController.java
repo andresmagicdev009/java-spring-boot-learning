5fbac1f042db148324bb9c762d7f5a86c23da1ed
andresmagicdev009 <andres.rodriguezso09@gmail.com>
2026-08-26 19:52:30 -0500

    Learning @Values annotation

--- src/main/java/com/nttdata/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/nttdata/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package com.nttdata.curso.springboot.webapp.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nttdata.curso.springboot.webapp.springboot_web.models.User;
+
 import com.nttdata.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+import com.nttdata.curso.springboot.webapp.springboot_web.models.dto.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -21,6 +25,18 @@ public class PathVariableController {
      * permite identificar recursos de manera unica y consistente.
      * ademas que es mas amigable para los motores de busqueda y para la navegacion de los usuarios. 
     */
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.message}")
+    private String message;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
+
 
     @GetMapping("/baz/{message}/{code}")
     public ParamDto baz(@PathVariable String message, @PathVariable Integer code) {
@@ -44,8 +60,21 @@ public class PathVariableController {
     @PostMapping("/create")
     public User createUser(@RequestBody User user){
         // Hacer algo con el usuario por ejemplo un save en la bdd
-        
+        user.setName(user.getName().toUpperCase());
+        user.setEmail(user.getEmail().toUpperCase());
+        user.setAge(user.getAge().toString().length() > 0 ? user.getAge() : 0);
         return user;
     }
+
+    @GetMapping("/values")
+    public Map<String, Object> values() {
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        json.put("code", code);
+        return json;
+    }
+    
     
 }
